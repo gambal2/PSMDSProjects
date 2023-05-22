0f0adacf6ceadaee9a18ed3e78ee891c60b0433d
gambal2 <96159167+gambal2@users.noreply.github.com>
2023-05-22 09:14:55 +0800

    Add files via upload

---
+++ Finals_AGamboa.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+#Simple Stock Price App
+
+Shown are thestock closing price and volume of Google!
+
+""")
+
+
+tickerSymbol = 'GOOGL'
+
+tickeData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period = '1d', start = '2010-5-31' , end '2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
